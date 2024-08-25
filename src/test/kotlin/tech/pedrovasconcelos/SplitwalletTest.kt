7bd116e5fe0872c9ea310e9e5636396ee8e6a6d1
pedro.vasconcelos <pedrohnvasconcelos@gmail.com>
2024-08-24 23:15:57 -0300

    first commit

---
+++ src/test/kotlin/tech/pedrovasconcelos/SplitwalletTest.kt
@@ -0,0 +1,19 @@
+package tech.pedrovasconcelos
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class SplitwalletTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
